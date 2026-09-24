88e55e9e541e104e897932560e1d83be46c7584d
Martin Moen <mmoen726@gmail.com>
2026-09-23 22:09:18 -0500

    Add SLA breach detection and ticket dashboard public page

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ const ticketsRouter = require('./routes/tickets');
 
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/tickets', ticketsRouter);
 
-app.get('/', (req, res) => {
-    res.send('ITSM API is running with CI/CD pipeline!');
-});
+//app.get('/', (req, res) => {
+//    res.send('ITSM API is running with CI/CD pipeline!');
+//});
 
 app.use((err, req, res, _next) => {
     console.error(err);
